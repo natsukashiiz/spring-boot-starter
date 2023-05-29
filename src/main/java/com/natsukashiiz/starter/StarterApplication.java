73fe836d78bebe4827b3bcb2991b4d5fed88fcdc
ᴡᴇᴇʀᴀᴡᴀᴛ s <74323401+WEERAWATs@users.noreply.github.com>
2023-05-29 21:23:40 +0700

    merge file

--- src/main/java/com/natsukashiiz/starter/StarterApplication.java
+++ src/main/java/com/natsukashiiz/starter/StarterApplication.java
@@ -2,12 +2,19 @@ package com.natsukashiiz.starter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class StarterApplication {
+public class StarterApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(StarterApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(StarterApplication.class);
     }
 
 }
