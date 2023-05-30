d5683f5c5043b438f75ea076e88c6d76229fd73d
ᴡᴇᴇʀᴀᴡᴀᴛ s <74323401+WEERAWATs@users.noreply.github.com>
2023-05-30 23:26:46 +0700

    Update ResponseCode.java

--- src/main/java/com/natsukashiiz/starter/common/ResponseCode.java
+++ src/main/java/com/natsukashiiz/starter/common/ResponseCode.java
@@ -1,39 +1,35 @@
 package com.natsukashiiz.starter.common;
-import java.util.Objects;
+
 import java.util.Optional;
 
-public enum ResponseCode implements StateMapping<Integer> {
-
-    /** success */
+public enum ResponseCode {
     SUCCESS(0),
+    INVALID_REQUEST(4010),
+    INVALID_UID(4011),
+    INVALID_PASSWORD(4013),
+    INVALID_USERNAME_PASSWORD(4012),
+    INVALID_CODE(4013),
+    NOT_FOUND(4040),
+    TOKEN_EXPIRE(4070),
+    REFRESH_TOKEN_EXPIRE(4071),
+    UNKNOWN(9999);
 
-    /** token expire */
-    TOKEN_EXPIRE(4001),
+    private final Integer value;
 
-    /** unknown error */
-    UNKNOWN(9999),
-    ;
-
-    private final Integer state;
-
-    ResponseCode(Integer state) {
-        this.state = state;
+    ResponseCode(final Integer code) {
+        this.value = code;
     }
 
-    public static Optional<ResponseCode> find(Integer code){
-        // if code null throw error
-        Objects.requireNonNull(code);
-        for (ResponseCode value : ResponseCode.values()) {
-            if(value.is(code)){
-                return Optional.of(value);
-            }
+    public Integer getValue() {
+        return value;
+    }
+
+    public static Optional<ResponseCode> find(Integer code) {
+        for (ResponseCode values : ResponseCode.values()) {
+            if (values.value.equals(code))
+                return Optional.of(values);
         }
         return Optional.empty();
-    }
-
-    @Override
-    public Integer getMapping() {
-        return this.state;
     }
 }
 
