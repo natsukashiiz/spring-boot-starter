5ae25fadaaedd915952b27125bcc00aebf464455
ᴡᴇᴇʀᴀᴡᴀᴛ s <74323401+WEERAWATs@users.noreply.github.com>
2023-05-30 23:26:57 +0700

    Create ExampleResponse.java

---
+++ src/main/java/com/natsukashiiz/starter/model/response/ExampleResponse.java
@@ -0,0 +1,34 @@
+package com.natsukashiiz.starter.model.response;
+
+public class ExampleResponse {
+    private String what;
+    private String who;
+    private String where;
+
+    public String getWhat() {
+        return what;
+    }
+
+    public ExampleResponse setWhat(String what) {
+        this.what = what;
+        return this;
+    }
+
+    public String getWho() {
+        return who;
+    }
+
+    public ExampleResponse setWho(String who) {
+        this.who = who;
+        return this;
+    }
+
+    public String getWhere() {
+        return where;
+    }
+
+    public ExampleResponse setWhere(String where) {
+        this.where = where;
+        return this;
+    }
+}
