f88d266c78eed78e8aefb83c2b6f332aa2a40218
ᴡᴇᴇʀᴀᴡᴀᴛ s <74323401+WEERAWATs@users.noreply.github.com>
2023-06-05 13:05:25 +0700

    Create PrivateApi.java

---
+++ src/main/java/com/natsukashiiz/starter/controller/PrivateApi.java
@@ -0,0 +1,16 @@
+package com.natsukashiiz.starter.controller;
+
+import com.natsukashiiz.starter.model.Response;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/v1/private")
+public class PrivateApi {
+    @GetMapping
+    public ResponseEntity<?> get() {
+        return Response.success();
+    }
+}
