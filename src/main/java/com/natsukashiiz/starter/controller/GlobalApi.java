04fa3cb48617b3fab08bc43c69cd7dc855c1a29a
ᴡᴇᴇʀᴀᴡᴀᴛ s <74323401+WEERAWATs@users.noreply.github.com>
2023-06-05 13:05:20 +0700

    Create GlobalApi.java

---
+++ src/main/java/com/natsukashiiz/starter/controller/GlobalApi.java
@@ -0,0 +1,16 @@
+package com.natsukashiiz.starter.controller;
+
+import com.natsukashiiz.starter.model.Response;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/v1/global")
+public class GlobalApi {
+    @GetMapping
+    public ResponseEntity<?> get() {
+        return Response.success();
+    }
+}
