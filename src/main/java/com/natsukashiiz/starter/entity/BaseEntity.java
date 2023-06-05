baf8efdcad88e983395facaf93b2ca132ad1236d
ᴡᴇᴇʀᴀᴡᴀᴛ s <74323401+WEERAWATs@users.noreply.github.com>
2023-06-05 12:50:11 +0700

    Create BaseEntity.java

---
+++ src/main/java/com/natsukashiiz/starter/entity/BaseEntity.java
@@ -0,0 +1,24 @@
+package com.natsukashiiz.starter.entity;
+
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+@Data
+@MappedSuperclass
+public class BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @CreationTimestamp
+    private LocalDateTime cdt;
+    @UpdateTimestamp
+    private LocalDateTime udt;
+}
