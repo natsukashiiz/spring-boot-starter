2c2b235e6cc0e2ce51f22063cffe48396402168e
ᴡᴇᴇʀᴀᴡᴀᴛ s <74323401+WEERAWATs@users.noreply.github.com>
2023-06-05 12:51:02 +0700

    Create AuthTokenFilter.java

---
+++ src/main/java/com/natsukashiiz/starter/security/jwt/AuthTokenFilter.java
@@ -0,0 +1,70 @@
+package com.natsukashiiz.starter.security.jwt;
+
+import com.natsukashiiz.starter.service.UserDetailsServiceImpl;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Objects;
+
+public class AuthTokenFilter extends OncePerRequestFilter {
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+    @Autowired
+    private JwtUtils jwtUtils;
+    @Autowired
+    private UserDetailsServiceImpl userDetailsService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        try {
+            String jwt = parseJwt(request);
+            if (Objects.nonNull(jwt) && jwtUtils.validateJwtToken(jwt)) {
+                String username = jwtUtils.getUsernameFromToken(jwt);
+                logger.info("AuthTokenFilter-[doFilterInternal] username: {}", username);
+
+                UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+                if (Objects.isNull(userDetails)) {
+                    filterChain.doFilter(request, response);
+                    return;
+                }
+                logger.info("AuthTokenFilter-[doFilterInternal] userDetails: {}", userDetails.getUsername());
+
+                UsernamePasswordAuthenticationToken authentication =
+                        new UsernamePasswordAuthenticationToken(
+                                userDetails,
+                                null,
+                                null
+                        );
+                authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            }
+        } catch (Exception e) {
+            logger.error("AuthTokenFilter-[Exception] error: {}", e.getMessage());
+        }
+
+        filterChain.doFilter(request, response);
+    }
+
+    private String parseJwt(HttpServletRequest request) {
+        String headerAuth = request.getHeader(HttpHeaders.AUTHORIZATION);
+        String tokenPrefix = "Bearer ";
+
+        if (StringUtils.hasText(headerAuth) && headerAuth.startsWith(tokenPrefix)) {
+            return headerAuth.replace(tokenPrefix, "");
+        }
+
+        return null;
+    }
+}
