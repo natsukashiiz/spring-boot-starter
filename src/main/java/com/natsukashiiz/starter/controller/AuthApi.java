64dbad553c965cbeac6918187bca785742ecf998
ᴡᴇᴇʀᴀᴡᴀᴛ s <74323401+WEERAWATs@users.noreply.github.com>
2023-06-05 15:05:29 +0700

    added refresh token api

--- src/main/java/com/natsukashiiz/starter/controller/AuthApi.java
+++ src/main/java/com/natsukashiiz/starter/controller/AuthApi.java
@@ -1,8 +1,8 @@
 package com.natsukashiiz.starter.controller;
 
-import com.natsukashiiz.starter.model.Response;
 import com.natsukashiiz.starter.model.request.LoginRequest;
 import com.natsukashiiz.starter.model.request.RegisterRequest;
+import com.natsukashiiz.starter.model.request.TokenRefreshRequest;
 import com.natsukashiiz.starter.service.UserService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -30,8 +30,8 @@ public class AuthApi {
     }
 
     @PostMapping("/refresh")
-    public ResponseEntity<?> refresh() {
-        return Response.success();
+    public ResponseEntity<?> refresh(@RequestBody TokenRefreshRequest request) {
+        return service.refreshToken(request);
     }
 
 }
