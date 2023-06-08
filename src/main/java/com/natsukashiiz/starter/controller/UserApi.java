57940d7f44fad37bed1c74f2f817dea919a5118b
vv2dev <114040584+vv2dev@users.noreply.github.com>
2023-06-08 13:15:27 +0700

    added signedHistory api

--- src/main/java/com/natsukashiiz/starter/controller/UserApi.java
+++ src/main/java/com/natsukashiiz/starter/controller/UserApi.java
@@ -1,21 +1,29 @@
 package com.natsukashiiz.starter.controller;
 
+import com.natsukashiiz.starter.model.Pagination;
 import com.natsukashiiz.starter.model.request.ChangePasswordRequest;
 import com.natsukashiiz.starter.model.request.UpdateUserRequest;
 import com.natsukashiiz.starter.service.UserService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/v1/users")
 @RequiredArgsConstructor
+@Slf4j
 public class UserApi {
     private final UserService service;
 
     @GetMapping
     public ResponseEntity<?> get() {
         return service.getMe();
+    }
+
+    @GetMapping("/signedHistory")
+    public ResponseEntity<?> signedHistory(Pagination pagination) {
+        return service.signedHistory(pagination);
     }
 
     @PatchMapping
