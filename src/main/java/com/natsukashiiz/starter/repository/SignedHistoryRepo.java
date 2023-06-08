2203b5ad5bfadff089d442c80c7998f471eaf4aa
vv2dev <114040584+vv2dev@users.noreply.github.com>
2023-06-08 13:15:49 +0700

    Update SignedHistoryRepo.java

--- src/main/java/com/natsukashiiz/starter/repository/SignedHistoryRepo.java
+++ src/main/java/com/natsukashiiz/starter/repository/SignedHistoryRepo.java
@@ -1,7 +1,11 @@
 package com.natsukashiiz.starter.repository;
 
 import com.natsukashiiz.starter.entity.SignedHistory;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface SignedHistoryRepo extends JpaRepository<SignedHistory, Long> {
+    List<SignedHistory> findAllByUid(long uid, Pageable pageable);
 }
